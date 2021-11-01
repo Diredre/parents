435c463738d8d1a13a8a5b3c92f248459880b6dd
Diredre <1677973750@qq.com>
2021-11-01 23:09:15 +0800

    优化2.0

--- app/src/main/java/com/example/parents/LoginRegister/RegisterActivity.java
+++ app/src/main/java/com/example/parents/LoginRegister/RegisterActivity.java
@@ -19,11 +19,11 @@ import com.example.parents.Widget.Code;
 
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private ImageView reg_iv_phone, reg_iv_password, reg_iv_code, reg_iv_logo, reg_iv_phcode;
+    private ImageView reg_iv_phone, reg_iv_password, reg_iv_code, reg_iv_logo, reg_iv_repassword, reg_iv_phcode;
     private Button reg_btn_reg;
-    private EditText reg_et_phone, reg_et_code, reg_et_password;
-
+    private EditText reg_et_phone, reg_et_code, reg_et_password, reg_et_repassword;
     private String real_code;       // 生成验证码
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,16 +34,16 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         initView();
     }
 
-
     private void initView(){
         reg_et_phone = findViewById(R.id.reg_et_phone);
         reg_et_code = findViewById(R.id.reg_et_code);
         reg_et_password = findViewById(R.id.reg_et_password);
-        real_code = Code.getInstance().getCode().toLowerCase();
+        reg_et_repassword = findViewById(R.id.reg_et_repassword);
 
         reg_iv_phcode = findViewById(R.id.reg_iv_phcode);
         reg_iv_phcode.setImageBitmap(Code.getInstance().createBitmap());
         reg_iv_phcode.setOnClickListener(this);
+        real_code = Code.getInstance().getCode().toLowerCase();
 
         // reg按钮
         reg_btn_reg = findViewById(R.id.reg_btn_reg);
@@ -60,12 +60,17 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
                 .load("https://z3.ax1x.com/2021/11/01/IPS5on.png")
                 .into(reg_iv_code);
 
-        reg_iv_password = findViewById(R.id.login_iv_password);
+        reg_iv_password = findViewById(R.id.reg_iv_password);
         Glide.with(RegisterActivity.this)
                 .load("https://z3.ax1x.com/2021/10/24/5WrP2T.png")
                 .into(reg_iv_password);
 
-        reg_iv_logo = findViewById(R.id.login_iv_logo);
+        reg_iv_repassword = findViewById(R.id.reg_iv_repassword);
+        Glide.with(RegisterActivity.this)
+                .load("https://z3.ax1x.com/2021/10/24/5WrP2T.png")
+                .into(reg_iv_repassword);
+
+        reg_iv_logo = findViewById(R.id.reg_iv_logo);
         Glide.with(RegisterActivity.this)
                 .load("https://z3.ax1x.com/2021/10/24/5WK2jS.png")
                 .into(reg_iv_logo);
@@ -77,12 +82,38 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         switch (v.getId()){
             case R.id.reg_btn_reg:
                 String phoneCode = reg_et_code.getText().toString().toLowerCase();
-                if (phoneCode.equals(real_code)) {
-                    Toast.makeText(RegisterActivity.this, phoneCode + "注册成功！", Toast.LENGTH_SHORT).show();
-                    startActivity(new Intent(RegisterActivity.this, MainActivity.class));
-                    RegisterActivity.this.finish();
+                String phone = reg_et_phone.getText().toString().trim();
+                String psw = reg_et_password.getText().toString().trim();
+                String repsw = reg_et_repassword.getText().toString().trim();
+
+                if(phone.equals("")){
+                    Toast.makeText(RegisterActivity.this, "请输入手机号码", Toast.LENGTH_SHORT).show();
+                    reg_iv_phcode.setImageBitmap(Code.getInstance().createBitmap());
+                    real_code = Code.getInstance().getCode().toLowerCase();
+                } else if(psw.equals("")){
+                    Toast.makeText(RegisterActivity.this, "请输入密码", Toast.LENGTH_SHORT).show();
+                    reg_iv_phcode.setImageBitmap(Code.getInstance().createBitmap());
+                    real_code = Code.getInstance().getCode().toLowerCase();
+                } else if(repsw.equals("")){
+                    Toast.makeText(RegisterActivity.this, "请再次输入密码", Toast.LENGTH_SHORT).show();
+                    reg_iv_phcode.setImageBitmap(Code.getInstance().createBitmap());
+                    real_code = Code.getInstance().getCode().toLowerCase();
+                } else if (psw.equals(repsw)){
+                    Toast.makeText(RegisterActivity.this, "两次密码请保持一致", Toast.LENGTH_SHORT).show();
+                    reg_iv_phcode.setImageBitmap(Code.getInstance().createBitmap());
+                    real_code = Code.getInstance().getCode().toLowerCase();
+                }
+
+                if(phoneCode.equals(real_code)) {
+                    Toast.makeText(RegisterActivity.this, "请输入验证码", Toast.LENGTH_SHORT).show();
+                }else if (phoneCode.equals(real_code)) {
+                        Toast.makeText(RegisterActivity.this, "注册成功", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+                        RegisterActivity.this.finish();
                 } else {
-                    Toast.makeText(RegisterActivity.this, phoneCode + "验证码错误", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(RegisterActivity.this, "验证码错误", Toast.LENGTH_SHORT).show();
+                    reg_iv_phcode.setImageBitmap(Code.getInstance().createBitmap());
+                    real_code = Code.getInstance().getCode().toLowerCase();
                 }
                 break;
             case R.id.reg_iv_phcode:
